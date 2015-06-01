8df56fdd4a0eab6605d413fd2ffee1da6f9a0683
Michael Emmons <mge.temp@gmail.com>
2015-06-01 11:02:51 -0700

    Initial Commit

---
+++ Columns/Columns.h
@@ -0,0 +1,19 @@
+//
+//  Columns.h
+//  Columns
+//
+//  Created by Michael Emmons on 6/1/15.
+//  Copyright (c) 2015 Michael Emmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Columns.
+FOUNDATION_EXPORT double ColumnsVersionNumber;
+
+//! Project version string for Columns.
+FOUNDATION_EXPORT const unsigned char ColumnsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Columns/PublicHeader.h>
+
+
